4bc7d174707899d2ac93b0e97648a1fc491ac6d5
Luis Capelo <luiscape@gmail.com>
2014-11-06 16:43:10 -0500

    fixing relative path

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@
 source ~/venv/bin/activate
 
 # running tests
-python ~/tests/tests.py a02903a9-022b-4047-bbb5-45127b591c85 API_KEY
+python ~/tool/tests/tests.py a02903a9-022b-4047-bbb5-45127b591c85 API_KEY
